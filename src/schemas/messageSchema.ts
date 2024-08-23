f1ccac8959dd78191ed8044a6f2e563273884b68
Dikshit Suthar <suthardikshit57@gmail.com>
2024-08-23 22:27:47 +0530

    dashboard created and other frontend is connected with backend

--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -1,6 +1,8 @@
-import {z} from "zod";
+import { z } from 'zod'
 
-export const acceptMessageSchema = z.object({
-  acceptMessage : z.string().min(10,"message must be at least 10 characters").max(300,"message must be at most 300 characters"),
-  
+export const messageSchema = z.object({
+  content: z
+    .string()
+    .min(10, { message: 'Content must be at least 10 characters.' })
+    .max(300, { message: 'Content must not be longer than 300 characters.' }),
 });
